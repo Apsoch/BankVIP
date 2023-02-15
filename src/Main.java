ec944dc13342864db911d3b52806e5a66b6d24fd
Anton Pantiukhin <jobapsoch@mail.ru>
2023-02-15 19:20:46 +0300

    Добавлен Main + отчет

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    int customerAccount = 2_000_000_000;
+    int transferAmount = 500_000_000;
+    int total = customerAccount + transferAmount;
+    System.out.println(total);
+  }
+}
